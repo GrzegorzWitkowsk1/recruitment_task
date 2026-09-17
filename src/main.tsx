622dacdf753b0e9a1d531495e1415836c51a9631
Grzegorz <grzegorz@motorro.pl>
2026-09-17 14:08:29 +0200

    feat: Basic project setup and styling config

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+
+import { NuqsAdapter } from "nuqs/adapters/react"
+
+import "./index.css"
+import MainView  from "@/views/MainView"
+import { ThemeProvider } from "@/components/theme-provider.tsx"
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <NuqsAdapter>
+        <MainView />
+      </NuqsAdapter>
+    </ThemeProvider>
+  </StrictMode>
+)
